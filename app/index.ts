da70033fb68974de8d0c2502f526c0de19cf530f
Adrian Fahrer (u123603) <adrian.fahrer@mobi.ch>
2024-08-08 16:09:43 +0200

    withLatestFrom examples

--- app/index.ts
+++ app/index.ts
@@ -5,7 +5,7 @@ import {concatMap, of, ReplaySubject, Subject, withLatestFrom} from "rxjs";
 const ex1Base$: Subject<number> = new Subject();
 const ex1wlf$: Subject<number> = new Subject();
 registerCounterButton("ex1-baseBtn", cnt => `base [${cnt}]`, ex1Base$);
-registerCounterButton("ex1-wlfBtn", cnt => `withLatestFrom(ReplaySubject) [${cnt}]`, ex1wlf$);
+registerCounterButton("ex1-wlfBtn", cnt => `withLatestFrom(Subject) [${cnt}]`, ex1wlf$);
 ex1Base$
   .pipe(withLatestFrom(ex1wlf$))
   .subscribe(([ex1Base, ex1wlf]: [number, number]) => write("ex1-out", `${ex1Base}-${ex1wlf}`));
@@ -14,7 +14,7 @@ ex1Base$
 const ex2Base$: Subject<number> = new Subject();
 const ex2wlf$: Subject<number> = new Subject();
 registerCounterButton("ex2-baseBtn", cnt => `base [${cnt}]`, ex2Base$);
-registerCounterButton("ex2-wlfBtn", cnt => `withLatestFrom(ReplaySubject) [${cnt}]`, ex2wlf$);
+registerCounterButton("ex2-wlfBtn", cnt => `withLatestFrom(Subject) [${cnt}]`, ex2wlf$);
 ex2Base$
   .pipe(concatMap(base => of(base).pipe(withLatestFrom(ex2wlf$))))
   .subscribe(([ex2Base, ex2wlf]: [number, number]) => write("ex2-out", `${ex2Base}-${ex2wlf}`));
@@ -29,7 +29,7 @@ ex3Base$
 .subscribe(([ex3Base, ex3wlf]: [number, number]) => write("ex3-out", `${ex3Base}-${ex3wlf}`));
 
 
-
+// ----- helpers -----
 function registerCounterButton(id: string, label: (counter: number) => string, targetSubject$: Subject<number>) {
   let counter = 0;
   document
